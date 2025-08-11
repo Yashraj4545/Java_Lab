6634efdb53114dc9fe72eb90d3b6e143e7d3df06
Yashraj4545 <kshirsagaryashraj50@gmail.com>
2025-08-11 13:32:27 +0530

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,9 +1,39 @@
-class Student{
-	int id = 13;
-	String name = "Yash";
+public class Student{
+	int roll_no;
+	String name;
+	int phone_no;
+	//int prn_no;
+	int marks;
+
+
+	public Student(){
+	int roll_no = 13;
+	name = "Yashraj";
+	phone_no = 99;
+	//prn_no = 0124UITM1013;
+	marks = 20;
+	System.out.println("roll no: "+roll_no+" name: "+name+" phone_no: "+phone_no+" marks: "+marks);
+	}
+	
+	public Student(int roll_no){
+	roll_no=roll_no;
+	System.out.println("roll no: "+roll_no);
+	}
+	public Student(int roll_no,String name){
+	roll_no=roll_no;
+	name=name;
+	System.out.println("roll no: "+roll_no+" name : "+name);
+	}
+	public Student(int roll_no,String name,int phone_no){
+	roll_no=roll_no;
+	name=name;
+	phone_no=phone_no;
+	System.out.println("roll no: "+roll_no+" name : "+name+" phone_no: "+phone_no);
+	}
 	public static void main(String args[]){
-		Student s1 = new Student();
-		System.out.println("ID: "+s1.id);
-		System.out.println("Name: "+s1.name);
+	Student s1 = new Student();
+	Student s2 = new Student(14);
+	Student s3 = new Student(22,"Gaurav");
+	Student s4 = new Student(7,"Krishnraj",98);
 	}
 }
--- Student01.java
+++ Student01.java
@@ -1,11 +1,18 @@
-class Student01{
-	Student01(){
-		System.out.print("No Argument Constructor");
+public class Student01{
+	int roll_no;
+	String name;
+	int marks;
+	public Student01(int roll_no,String name,int marks){
+	this.roll_no=roll_no;
+	this.name = name;
+	this.marks=marks;
+	}
+	public void Display(){
+	System.out.print("Roll no: "+roll_no+" Name : "+name+" marks: "+marks);
+	}
+	public static void main(String args[]){
+	Student01 s = new Student01(13,"Yashraj",20);
+	s.Display();
 	}
 
-		public static void main(String args[]){
-		Student01 s1 = new Student01();
-		}
-	
-	
 }

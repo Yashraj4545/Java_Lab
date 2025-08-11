095ae042aed175cd876e8f0fe3b509c84638243f
Yashraj4545 <kshirsagaryashraj50@gmail.com>
2025-08-11 13:25:20 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,9 @@
+class Student{
+	int id = 13;
+	String name = "Yash";
+	public static void main(String args[]){
+		Student s1 = new Student();
+		System.out.println("ID: "+s1.id);
+		System.out.println("Name: "+s1.name);
+	}
+}
